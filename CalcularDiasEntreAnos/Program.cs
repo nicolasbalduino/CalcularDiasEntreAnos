082f2aa63e03b607525ef80a223278e708a649e9
nicolasbalduino <nicolasbalduino@outlook.com>
2023-03-05 16:20:58 -0300

    Add project files.

---
+++ CalcularDiasEntreAnos/Program.cs
@@ -0,0 +1,15 @@
+﻿int anoInicial, anoFinal, qtdDias = 0;
+
+Console.WriteLine("Informe o ano inicial: ");
+anoInicial = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Informe o ano final: ");
+anoFinal = int.Parse(Console.ReadLine());
+
+for (int ano = anoInicial; ano < anoFinal; ano++)
+    if (ano % 400 == 0 || ((ano % 4 == 0) && (ano % 100 != 0)))
+        qtdDias += 1;
+
+qtdDias += (anoFinal - anoInicial) * 365;
+
+Console.WriteLine("Tiveram " + qtdDias + " dias.");
