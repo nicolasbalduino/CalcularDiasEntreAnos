ec0652e7e47a6bfb5fb3eeb28bbe967388b813d3
nicolasbalduino <nicolasbalduino@outlook.com>
2023-03-05 21:43:31 -0300

    criada funcao que testa se o ano informado e bissexto, se verdadeiro, adiciona 1 a quantidade de dias

--- CalcularDiasEntreAnos/Program.cs
+++ CalcularDiasEntreAnos/Program.cs
@@ -1,4 +1,10 @@
-﻿int anoInicial, anoFinal, qtdDias = 0;
+﻿int anoInicial, anoFinal, ano, qtdDias = 0;
+
+void AnoBissexto()
+{
+    if (ano % 400 == 0 || ((ano % 4 == 0) && (ano % 100 != 0)))
+        qtdDias += 1;
+}
 
 Console.WriteLine("Informe o ano inicial: ");
 anoInicial = int.Parse(Console.ReadLine());
@@ -6,9 +12,8 @@ anoInicial = int.Parse(Console.ReadLine());
 Console.WriteLine("Informe o ano final: ");
 anoFinal = int.Parse(Console.ReadLine());
 
-for (int ano = anoInicial; ano < anoFinal; ano++)
-    if (ano % 400 == 0 || ((ano % 4 == 0) && (ano % 100 != 0)))
-        qtdDias += 1;
+for (ano = anoInicial; ano < anoFinal; ano++)
+    AnoBissexto();
 
 qtdDias += (anoFinal - anoInicial) * 365;
 
